6693042d086197c7ee26b3f3b4bcc66283af94b5
Ita_Player_ <130795795+ItaPlayer09@users.noreply.github.com>
2024-12-17 16:39:03 +0100

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,7 +1,29 @@
 import os
 import time
-import pyautogui
+import pyautogui as py
 import pytesseract
+from PIL import Image
 
-url = input(¨Please give the item url: ")
-targetValue = input(int("Please give a target value: "))
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+url = input("Please give the item url: ")
+targetValue = input("Please give a target value: ")
+
+screenshot_area = (, , , )
+
+def MouseClick(coords):
+    py.click(coords)
+
+def CheckValue():
+    screen = py.screenshot(region=screenshot_area)
+    screen.save("img.png")
+    img = Image.open("img.png")
+    output = pytesseract.image_to_string(img)
+    print(output)
+    return output
+
+while True:
+    if CheckValue() == targetValue:
+        MouseClick((100, 100))
+        print("bought item")
+    time.sleep(0.1)
